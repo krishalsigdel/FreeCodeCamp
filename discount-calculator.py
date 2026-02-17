a81954714782d394bd22bde86131fd53d86b4dfc
krishal sigdel <krishalsigdel5@gmail.com>
2026-02-17 06:19:30 +0545

    innitial program

---
+++ discount-calculator.py
@@ -0,0 +1,18 @@
+
+def apply_discount(price, discount):
+    if not isinstance(price, (int, float)):
+        print( 'The price should be a number')
+    elif price <= 0:
+        print( 'The price should be greater than 0')   
+    elif not isinstance(discount, (int, float)):
+        print( 'The discount should be a number') 
+    elif price<=0:
+      print('The price should be greater than 0')
+    elif not (0 <= discount <= 100):
+        print('The discount should be between 0 and 100')
+    else:
+        discount=(discount/100)*price
+        total_cost=price-discount
+    print( 'total cost:', total_cost)
+apply_discount(100,20)
+
