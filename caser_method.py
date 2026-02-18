001e168698f010f5e0bd66817913749233ac5bb9
krishal sigdel <krishalsigdel5@gmail.com>
2026-02-18 17:57:50 +0545

    user input taken

---
+++ caser_method.py
@@ -0,0 +1,29 @@
+def caesar(user_need,text, shift):
+    maxi=len(text)
+    if not isinstance(shift, int):
+        return 'Shift must be an integer value.'
+    elif not(shift > 1 or shift < maxi):
+        return 'Shift must be an integer between 1 and '+ str(maxi)
+    else:
+        if user_need=="D":
+            shift=-shift
+
+        alphabet = 'abcdefghijklmnopqrstuvwxyz'    
+        shifted_alphabet = alphabet[shift:] + alphabet[:shift]
+        translation_table = str.maketrans(alphabet + alphabet.upper(), shifted_alphabet + shifted_alphabet.upper())
+        encrypted_text = text.translate(translation_table)
+        return encrypted_text
+
+user_need=input('do you wanna encrypt(E) or decrypt(D)')
+user_need=user_need.upper()
+if user_need=="E":
+    text=input('Enter the text that needs to be encrypted :     ')
+    shift=int(input('write the shift that needs to be done(this should not be changed while doing vice versa)'))
+    print (caesar(user_need, text ,shift))
+elif user_need=='D':
+    text=input('Enter the text that needs to be decrepted:     ')
+    shift=int(input('write the shift that needs to be done(this should not be changed while doing vice versa)'))
+    print (caesar(user_need, text ,shift))
+else:
+    print('you need to enter either "E" or "D"')
+    
